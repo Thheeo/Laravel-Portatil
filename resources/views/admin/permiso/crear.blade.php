8070334d9c426a3fd1bad2c9aa75f8bffe9fce58
TDM Teo <nautilius92@gmail.com>
2020-02-02 16:12:27 -0500

    Commit Inicial del proyecto

---
+++ resources/views/admin/permiso/crear.blade.php
@@ -0,0 +1,21 @@
+@extends("theme.$theme.layout")
+
+@section('titulo')
+    permiso
+@endsection
+
+
+@section('contenido')
+<div class="row">
+    <div class="col-lg-12">
+      <div class="box box-danger">
+        <div class="box-header with-border">
+          <h3 class="box-title">Crear Permiso</h3>
+        </div>
+        <div class="box-body">
+            Formulario de creacion
+        </div>
+      </div>
+    </div>
+</div>
+@endsection
